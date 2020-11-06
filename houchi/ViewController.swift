c396edc009987f936b557de3fec98a56861c05f2
naru <mibo55rui@gmail.com>
2020-11-06 19:41:35 +0900

    Initial Commit

---
+++ houchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  houchi
+//
+//  Created by 橋目　那桜 on 2020/11/06.
+//  Copyright © 2020 橋目　那桜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
