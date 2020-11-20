973437c86bd5427d7a1fca92f966b592b9438d00
naru <mibo55rui@gmail.com>
2020-11-20 20:34:56 +0900

    2020.11.20_2

--- houchi/ViewController.swift
+++ houchi/ViewController.swift
@@ -14,25 +14,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // 背景画像に設定したい画像を定義
-        let image = UIImage(named: "1")!
-        
-        // ラベルの大きさに画像を調整！
-        let resizedImage = image.resized(toWidth: m.frame.width)
-        
-        // ラベルの背景に画像を設定！
-        m.backgroundColor = UIColor(patternImage: resizedImage!)
     }
 
 
 }
-extension UIImage {
-    func resized(toWidth width: CGFloat) -> UIImage? {
-        let canvasSize = CGSize(width: width, height: CGFloat(ceil(width / size.width * size.height)))
-        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
-        defer { UIGraphicsEndImageContext() }
-        draw(in: CGRect(origin: .zero, size: canvasSize))
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-}
+
