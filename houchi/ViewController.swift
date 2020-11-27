98aff8c5b63b100bee6df8ed1e0fcbd80da80d4a
naru <mibo55rui@gmail.com>
2020-11-27 19:45:10 +0900

    11.27

--- houchi/ViewController.swift
+++ houchi/ViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var m: UILabel!
+    var number: Int = 0
+    @IBOutlet var a: UILabel!
+      @IBOutlet var b: UILabel!
+      @IBOutlet var c: UILabel!
+      @IBOutlet var d: UILabel!
+      @IBOutlet var e: UILabel!
+    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       let timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(self.count),
+            userInfo: nil,
+            repeats: true     )
+        
+    }
+    @objc func count() {
+        number = number + 1
+        d.text = String(number)
     }
 
 
