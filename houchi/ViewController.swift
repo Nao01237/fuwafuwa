fdba2c864cfe865ec93df1c282ca1ac181e9cdbe
naru <mibo55rui@gmail.com>
2020-12-04 20:34:34 +0900

    12.4

--- houchi/ViewController.swift
+++ houchi/ViewController.swift
@@ -10,17 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     var number: Int = 0
+    var level: Int = 1
+    var timer: Timer?
     @IBOutlet var a: UILabel!
       @IBOutlet var b: UILabel!
       @IBOutlet var c: UILabel!
       @IBOutlet var d: UILabel!
       @IBOutlet var e: UILabel!
+      @IBOutlet var f: UILabel!
     
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       let timer = Timer.scheduledTimer(
+        timer = Timer.scheduledTimer(
             timeInterval: 1,
             target: self,
             selector: #selector(self.count),
@@ -29,10 +32,30 @@ class ViewController: UIViewController {
         
     }
     @objc func count() {
+        if number >= 10 {
+            timer?.invalidate()
+        } else {
         number = number + 1
-        d.text = String(number)
+        d.text = "\(String(number))円"
+        }
+        
+    }
+       @IBAction func levelUp() {
+        if number > 5 {
+            number = number - 5
+            level  = level + 1
+            f.text = String(level)
+            
+
+            
+        }
+            
+            
+        }
+    
+    
     }
 
 
-}
+
 
