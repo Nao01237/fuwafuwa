e4c9cf48f035a26df213cbae1759ca4eafd5cd64
naru <mibo55rui@gmail.com>
2021-02-26 20:12:16 +0900

    ２０２１.２.２６

--- houchi/ViewController.swift
+++ houchi/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     var level: Int = 1
     var maibilyou: Int = 3
     var timer: Timer?
+    var cost: Int = 0
     @IBOutlet var a: UILabel!
     @IBOutlet var b: UILabel!
     @IBOutlet var c: UILabel!
@@ -22,31 +23,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let shozikinn = UserDefaults.standard.object(forKey: "shozikinn")
-        if shozikinn != nil {
-        } else {
-            // UserDefaultsに所持金を保存するために初期化
-                  UserDefaults.standard.set(0, forKey: "shozikinn")
         
-    }
-      
+        let appDomain = Bundle.main.bundleIdentifier
+        UserDefaults.standard.removePersistentDomain(forName: appDomain!)
+        let shozikinn = UserDefaults.standard.object(forKey: "shozikinn")
+        if shozikinn == nil {
+            // UserDefaultsに所持金を保存するために初期化
+            UserDefaults.standard.set(0, forKey: "shozikinn")
+        }
+
         timer = Timer.scheduledTimer(
             timeInterval: 1,
             target: self,
             selector: #selector(self.count),
             userInfo: nil,
             repeats: true     )
-        a.text = "毎秒  \(String(maibilyou))"
-        g.text = "\(String(maibilyou * 25))円"
         let l = UserDefaults.standard.object(forKey: "level")
-        if l != nil {
-            level = l as! Int
-        } else {
+        if l == nil {
+            UserDefaults.standard.set(1, forKey: "level")
         }
         f.text = String(level)
         maibilyou = 3 * level
         a.text = "毎秒　\(String(maibilyou))"
-        g.text = "\(String(level * 25))円"
+        cost = maibilyou * 25
+        g.text = "\(String(cost))円"
+        
         
     }
     
@@ -65,20 +66,21 @@ class ViewController: UIViewController {
     
     @IBAction func levelUp() {
         var shozikinn = UserDefaults.standard.object(forKey: "shozikinn") as! Int
-        if shozikinn >  maibilyou * 25 {
+        if shozikinn >= cost {
             // 所持金を引いて保存
-            shozikinn = shozikinn - maibilyou
+            shozikinn = shozikinn - cost
             UserDefaults.standard.set(shozikinn, forKey: "shozikinn")
             level = level + 1
-            maibilyou = (maibilyou + 10) * 2
+            maibilyou = 3 * level
+            cost = maibilyou * 25
             UserDefaults.standard.set(level, forKey: "level")
             f.text = String(level)
-            g.text = "\(String(maibilyou * 25))円"
+            g.text = "\(String(cost))円"
             a.text = "毎秒 　\(String(maibilyou))"
         }
+    
     }
 }
-
 
 
 
